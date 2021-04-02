6052bece8e16fef48a0d21031d14d28f9f4968f6
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-02 20:38:39 +0900

    基本機能実装

--- public/script.js
+++ public/script.js
@@ -4,52 +4,53 @@ new Vue({
     todos: [],
     new_todo: null
   },
-  created(){
-    this.read()
+  created() {
+    this.read();
   },
   methods: {
-
     //一覧描画
     //fetchでHTTP通信をする
-    read(){
-      fetch('http://localhost:3000/todos')
-        .then( res => res.join() )
-        .then( res => this.todos = res )
+    read() {
+      fetch("http://localhost:3000/todos")
+        .then(res => res.join())
+        .then(res => (this.todos = res));
     },
     //新規追加
-    todoCreate(){
-      fetch('http://localhost:3000/todos' {
-
-      })
+    todoCreate() {
+      fetch("http://localhost:3000/todos", {
+        methods: "POST",
+        body: JSON.stringify({
+          title: this.new_todo
+        }),
+        headers: new Headers({ "Content-type": "application/json" })
+      }).then(() => {
+        this.todos.push({
+          title: this.new_todo
+        });
+        this.new_todo = "";
+      });
     },
-     todoEdit: function {
-    //  入力内容を上書き(.push)
-    }
-    todoDelete: {
-      // 保存されている内容を削除(.splice)
-    }
-    todoCreate: {
-      投稿内容を削除
+
+    //編集
+    todoEdit(todo) {
+      fetch(`http://localhost:3000/todos/${todo.id}`, {
+        method: "PUT",
+        body: JSON.stringify({
+          title: todo.title
+        }),
+        headers: new Headers({
+          "Content-type": "application/json"
+        })
+      });
+    },
+
+    //削除
+    todoDelete(todo) {
+      fetch(`http://localhost:3000/todos/${todo.id}`, {
+        method: "DELETE"
+      }).then(() => {
+        this.todos = this.todos.filter(item => item !== todo);
+      });
     }
   }
 });
-
-// new Vue({
-//   el: "#app",
-//   date: {
-//     todos: {
-//       title: ""
-//     },
-//     methods: {
-//       todoEdit: {
-//         入力内容を上書き(.push)
-//       },
-//       todoDelete: {
-//         保存されている内容を削除(.splice)
-//       },
-//       todoCreate: {
-
-//       }
-//     }
-//   }
-// });
