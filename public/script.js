bcf0df6426855361f307030d9eba643bf955e09b
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-02 20:02:45 +0900

    一覧表示 read完成

--- public/script.js
+++ public/script.js
@@ -1,19 +1,55 @@
 new Vue({
   el: "#app",
   date: {
-    todos: {
-      title: ""
-    },
-    methods: {
-      todoEdit: {
-        入力内容を上書き(.push)
-      },
-      todoDelete: {
-        保存されている内容を削除(.splice)
-      },
-      todoCreate: {
+    todos: [],
+    new_todo: null
+  },
+  created(){
+    this.read()
+  },
+  methods: {
 
-      }
+    //一覧描画
+    //fetchでHTTP通信をする
+    read(){
+      fetch('http://localhost:3000/todos')
+        .then( res => res.join() )
+        .then( res => this.todos = res )
+    },
+    //新規追加
+    todoCreate(){
+      fetch('http://localhost:3000/todos' {
+
+      })
+    },
+     todoEdit: function {
+    //  入力内容を上書き(.push)
+    }
+    todoDelete: {
+      // 保存されている内容を削除(.splice)
+    }
+    todoCreate: {
+      投稿内容を削除
     }
   }
 });
+
+// new Vue({
+//   el: "#app",
+//   date: {
+//     todos: {
+//       title: ""
+//     },
+//     methods: {
+//       todoEdit: {
+//         入力内容を上書き(.push)
+//       },
+//       todoDelete: {
+//         保存されている内容を削除(.splice)
+//       },
+//       todoCreate: {
+
+//       }
+//     }
+//   }
+// });
